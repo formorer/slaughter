940ad99c3911bc8632148343ee38e0fc22e7bd19
Steve Kemp <steve@steve.org.uk>
2010-01-16 23:03:45 +0000

    Moved internal-only code into a sub-module.

---
+++ lib/Slaughter/Private.pm
@@ -0,0 +1,115 @@
+#!/usr/bin/perl -w
+
+=head1 NAME
+
+Slaughter::Private - Perl Automation Tool Helper Internal Details
+
+=cut
+
+=head1 SYNOPSIS
+
+This module implements the non-public API of the Slaughter administration tool.
+
+=cut
+
+
+=head1 AUTHOR
+
+ Steve
+ --
+ http://www.steve.org.uk/
+
+=cut
+
+=head1 LICENSE
+
+Copyright (c) 2010 by Steve Kemp.  All rights reserved.
+
+This module is free software;
+you can redistribute it and/or modify it under
+the same terms as Perl itself.
+The LICENSE file contains the full text of the license.
+
+=cut
+
+
+
+use LWP::UserAgent;
+use Digest::SHA1;
+use File::Basename qw/ dirname /;
+use File::Temp qw/ tempfile /;
+use File::Path qw/ mkpath /;
+use Text::Template;
+
+
+##
+##  PRIVATE
+##
+sub fetchURL
+{
+    my ($url) = (@_);
+
+    $verbose && print "\tfetchURL( $url) \n";
+
+    my $ua = LWP::UserAgent->new();
+    $ua->env_proxy();
+
+
+    #
+    #  Make requests for:
+    #
+    #  url.$fqdn
+    #  url.$hostname
+    #  url
+    #
+    #  Return the first one that matches, if any do.
+    #
+    my $response = $ua->get( $url . "." . $fqdn );
+    if ( $response->is_success() )
+    {
+        $verbose && print "\t$url.$fqdn OK\n";
+        return ( $response->decoded_content() );
+    }
+    $response = $ua->get( $url . "." . $hostname );
+    if ( $response->is_success() )
+    {
+        $verbose && print "\t$url.$hostname OK\n";
+        return ( $response->decoded_content() );
+    }
+
+    $response = $ua->get($url);
+    if ( $response->is_success() )
+    {
+        $verbose && print "\t$url OK\n";
+        return ( $response->decoded_content() );
+    }
+
+    #
+    #  Failed
+    #
+    $verbose && print "\tFailed to find content\n";
+    return undef;
+}
+
+
+
+##
+##  Private
+##
+sub checksumFile
+{
+    my ($file) = (@_);
+
+    my $sha1 = Digest::SHA1->new;
+    open my $handle, "<", $file;
+    $sha1->addfile($handle);
+    close($handle);
+
+    return ( $sha1->hexdigest() );
+
+}
+
+
+
+
+1;
--- lib/Slaughter/linux.pm
+++ lib/Slaughter/linux.pm
@@ -35,80 +35,12 @@ The LICENSE file contains the full text of the license.
 
 
 
-use LWP::UserAgent;
-use Digest::SHA1;
-use File::Basename qw/ dirname /;
-use File::Temp qw/ tempfile /;
-use File::Path qw/ mkpath /;
-use Text::Template;
+#
+#  The modules we use, and the internal functions are defined
+# in this module.
+#
+use Slaughter::Private;
 
-
-##
-##  PRIVATE
-##
-sub fetchURL
-{
-    my ($url) = (@_);
-
-    $verbose && print "\tfetchURL( $url) \n";
-
-    my $ua = LWP::UserAgent->new();
-    $ua->env_proxy();
-
-
-    #
-    #  Make requests for:
-    #
-    #  url.$fqdn
-    #  url.$hostname
-    #  url
-    #
-    #  Return the first one that matches, if any do.
-    #
-    my $response = $ua->get( $url . "." . $fqdn );
-    if ( $response->is_success() )
-    {
-        $verbose && print "\t$url.$fqdn OK\n";
-        return ( $response->decoded_content() );
-    }
-    $response = $ua->get( $url . "." . $hostname );
-    if ( $response->is_success() )
-    {
-        $verbose && print "\t$url.$hostname OK\n";
-        return ( $response->decoded_content() );
-    }
-
-    $response = $ua->get($url);
-    if ( $response->is_success() )
-    {
-        $verbose && print "\t$url OK\n";
-        return ( $response->decoded_content() );
-    }
-
-    #
-    #  Failed
-    #
-    $verbose && print "\tFailed to find content\n";
-    return undef;
-}
-
-
-
-##
-##  Private
-##
-sub checksumFile
-{
-    my ($file) = (@_);
-
-    my $sha1 = Digest::SHA1->new;
-    open my $handle, "<", $file;
-    $sha1->addfile($handle);
-    close($handle);
-
-    return ( $sha1->hexdigest() );
-
-}
 
 
 
