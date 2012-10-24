6e7293ad56f5f92a3a791a673f90d90d885529ec
Steve Kemp <steve@steve.org.uk>
2012-10-24 09:00:55 +0100

    stub constructor

---
+++ lib/Slaughter/Transport/git.pm
@@ -0,0 +1,67 @@
+=head1 NAME
+
+Slaughter::Transport::git - Transport class.
+
+=head1 SYNOPSIS
+
+...
+
+=cut
+
+=head1 DESCRIPTION
+
+This module contains code for dealing with a single registered site
+user.
+
+
+=cut
+
+
+package Slaughter::Transport::git;
+
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
+
+
+require Exporter;
+require AutoLoader;
+
+@ISA    = qw(Exporter AutoLoader);
+@EXPORT = qw();
+
+($VERSION) = '0.1';
+
+
+use strict;
+use warnings;
+
+
+
+=head2 new
+
+Create a new instance of this object.
+
+=cut
+
+sub new
+{
+    my ( $proto, %supplied ) = (@_);
+    my $class = ref($proto) || $proto;
+
+    my $self = {};
+
+    #
+    #  Allow user supplied values to override our defaults
+    #
+    foreach my $key ( keys %supplied )
+    {
+        $self->{ lc $key } = $supplied{ $key };
+    }
+
+    bless( $self, $class );
+    return $self;
+
+}
+
+
+
+
