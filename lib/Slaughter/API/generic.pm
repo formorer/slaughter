57dec0062b6055e8bf440da3d41223b753087d00
Steve Kemp <steve@steve.org.uk>
2012-10-25 04:22:21 +0100

    More POD updates.

--- lib/Slaughter/API/generic.pm
+++ lib/Slaughter/API/generic.pm
@@ -130,6 +130,44 @@ sub AppendIfMissing
 }
 
 
+=head2 CommentLinesMatching
+
+This primitive will open a local file, and comment out any line which matches
+the specified regular expression.
+
+=for example begin
+
+  if ( CommentLinesMatching( Pattern => "telnet|ftp",
+                             File    => "/etc/inetd.conf" ) )
+  {
+        RunCommand( Cmd => "/etc/init.d/inetd restart" );
+  }
+
+=for example end
+
+The following parameters are available:
+
+=over
+
+=item Comment [default: "#"]
+
+The value to comment out the line with.
+
+=item File [mandatory]
+
+The filename which should be examined and potentially updated.
+
+=item Pattern [mandatory]
+
+The regular expression to match with.
+
+=back
+
+The return value of this function is the number of lines updated,
+or -1 if the file could not be opened.
+
+=cut
+
 sub CommentLinesMatching
 {
     print "CommentLinesMatching - not implemented for $^O\n";
@@ -208,16 +246,134 @@ sub SetPermissions
 }
 
 
+=head2 RunCommand
+
+This primitive will execute a system command.
+
+=for example begin
+
+   RunCommand( Cmd => "/usr/bin/id" );
+
+=for example end
+
+The following parameters are available:
+
+=over
+
+=item Cmd [mandatory]
+
+The command to execute.  If no redirection is present in the command to execute
+then STDERR will be redirected to STDOUT automatically.
+
+=back
+
+The return value of this function is the result of the perl system function.
+
+=cut
+
+
 sub RunCommand
 {
     print "RunCommand - not implemented for $^O\n";
 }
 
 
+=head2 UserExists
+
+This primitive will test to see whether the given local user exists.
+
+=for example begin
+
+   if ( UserExists( User => "skx" ) )
+   {
+      # skx exists
+   }
+
+=for example end
+
+The following parameters are available:
+
+=over
+
+=item User [mandatory]
+
+The unix username to test for.
+
+=back
+
+The return value of this function is 1 if the user exists, and 0 otherwise.
+
+=cut
+
+
 sub UserExists
 {
     print "UserExists - not implemented for $^O\n";
 }
+
+
+=head2 UserDetails
+
+This primitive will return a hash of data about the local Unix user
+specified, if it exists.
+
+=for example begin
+
+   if ( UserExists( User => "skx" ) )
+   {
+      my %data = UserDetails( User => "skx" );
+   }
+
+=for example end
+
+The following parameters are available:
+
+=over
+
+=item User [mandatory]
+
+The unix username to retrieve details of.
+
+=back
+
+The return value of this function is a hash of data conprising of the
+following Keys/Values
+
+=over
+
+=item Home
+
+The user's home directory
+
+=item UID
+
+The user's UID
+
+=item GID
+
+The user's GID
+
+=item Quota
+
+The user's quota.
+
+=item Comment
+
+The user's comment
+
+=item Shell
+
+The user's login shell.
+
+=item Login
+
+The user's username.
+
+=back
+
+Undef will be returned on failure.
+
+=cut
 
 
 sub UserDetails
