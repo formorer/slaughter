2675d2bbd6d8596221f1be4be389b9d334d778e4
Steve Kemp <steve@steve.org.uk>
2012-10-26 19:40:54 +0100

    Move information about the platform out of our main code, and into the module.

--- lib/Slaughter/Info/linux.pm
+++ lib/Slaughter/Info/linux.pm
@@ -17,6 +17,99 @@ sub MetaInformation
     my ($ref) = (@_);
 
     #
+    #  Fully Qualified hostname
+    #
+    #  1.  If we can find /etc/hostname, then use that.
+    #
+    if ( -e "/etc/hostname" )
+    {
+        open( my $file, "<", "/etc/hostname" ) or
+          die "Failed to read /etc/hostname - $!";
+        $ref->{ 'fqdn' } = <$file>;
+        chomp( $ref->{ 'fqdn' } );
+        close($file);
+    }
+    else
+    {
+
+        #
+        #  Call "hostname".
+        #
+        $ref->{ 'fqdn' } = `hostname`;
+        chomp( $ref->{ 'fqdn' } );
+
+        #
+        # If it is unqualified retry with --fqdn.
+        #
+        if ( $ref->{ 'fqdn' } !~ /\./ )
+        {
+            $ref->{ 'fqdn' } = `hostname --fqdn`;
+            chomp( $ref->{ 'fqdn' } );
+        }
+    }
+
+
+    #
+    #  Get the hostname and domain name as seperate strings.
+    #
+    if ( $ref->{ 'fqdn' } =~ /^([^.]+)\.(.*)$/ )
+    {
+        $ref->{ 'hostname' } = $1;
+        $ref->{ 'domain' }   = $2;
+    }
+    else
+    {
+
+        #
+        #  Better than nothing, right?
+        #
+        $ref->{ 'hostname' } = $ref->{ 'fqdn' };
+        $ref->{ 'domain' }   = $ref->{ 'fqdn' };
+    }
+
+
+    #
+    #  Is this a xen host, or guest?
+    #
+    $ref->{ 'xen' } = 1 if -d "/proc/xen/capabilities";
+
+    #
+    #  KVM / Qemu?
+    #
+    if ( open( my $cpu, "<", "/proc/cpuinfo" ) )
+    {
+        foreach my $line (<$cpu>)
+        {
+            chomp($line);
+            $ref->{ 'kvm' } = 1 if ( $line =~ /model/ && $line =~ /qemu/i );
+        }
+        close($cpu);
+    }
+
+
+    #
+    #  Software RAID?
+    #
+    if ( ( -e "/proc/mdstat" ) &&
+         ( -x "/sbin/mdadm" ) )
+    {
+        if ( open( my $mdstat, "<", "/proc/mdstat" ) )
+        {
+            foreach my $line (<$mdstat>)
+            {
+                if ( ( $line =~ /^md([0-9]+)/ ) &&
+                     ( $line =~ /active/i ) )
+                {
+                    $ref->{ 'softwareraid' } = 1;
+                    $ref->{ 'raid' }         = "software";
+                }
+            }
+            close($mdstat);
+        }
+    }
+
+
+    #
     #  Kernel version.
     #
     $ref->{ 'kernel' } = `uname -r`;
