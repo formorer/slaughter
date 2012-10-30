67028714d36fc7008d8f1e39cd627764f21db754
Steve Kemp <steve@steve.org.uk>
2012-10-30 21:28:32 +0000

    Allow loading of Slaughter::API::Local:: if present, to allow   user-supplied primitives.

--- lib/Slaughter.pm
+++ lib/Slaughter.pm
@@ -21,8 +21,8 @@ It is loaded via:
 =for example end
 
 This usage actually dynamically loads the appropriate module from beneath the
-Slaughter::API namespace - which will contain the implementation of the Slaughter
-primitives.
+Slaughter::API namespace - which will contain the implementation of the primitives
+distributed with Slaughter.
 
 If no appropriate module is located to match the current system then the fall-backup
 module (Slaughter::API::generic) is loaded.  The generic module contains stub
@@ -39,6 +39,15 @@ upon the primitives which need to be implemented or adapted.
 
 =cut
 
+=head1 EXTENSIONS
+
+At the same time as loading the appropriate module from beneath the
+Slaughter::API name-space this module will attempt to load an identically
+named module from beneath the Slaughter::API::Local namespace.
+
+This allows you to keep develop your own custom-primitives.
+
+=cut
 
 =head1 AUTHOR
 
@@ -75,10 +84,29 @@ BEGIN
     eval($specific);
     ## use critic
 
-    # if there were errors then we'll use the generic
-    # handler
-    if ($@)
+    #
+    # If there were no errors we'll look for a local extension module too:
+    #
+    if ( !$@ )
     {
+        my $ext = "use Slaughter::API::Local::$^O;";
+
+        ## no critic (Eval)
+        eval($ext);
+        ## use critic
+
+        #
+        #  We don't care if the local-module fails to load because we expect
+        # it to be missing in the general case.
+        #
+    }
+    else
+    {
+
+        #
+        #  We found errors so we'll fall-back to the generic implementation
+        #
+
         ## no critic (Eval)
         eval($generic);
         ## use critic
